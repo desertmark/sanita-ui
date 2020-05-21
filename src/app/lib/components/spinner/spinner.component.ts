0e237685472d3a566e6837b0a8b067fd89aa80fc
Fernando Asulay <fer.guitar.ln@gmail.com>
2020-05-21 17:58:59 +0200

    spinner component

--- src/app/lib/components/spinner/spinner.component.ts
+++ src/app/lib/components/spinner/spinner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-spinner',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./spinner.component.scss']
 })
 export class SpinnerComponent implements OnInit {
-
+  @Input() fullScreen = false;
+  @Input() overlay = false;
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  get position(): string {
+    return this.fullScreen ? 'fixed' : 'absolute';
+  }
+
 
 }
