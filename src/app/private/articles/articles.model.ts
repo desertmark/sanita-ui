3276fe00222cb1c9e6277cb34a9a0be7066ca178
Fernando Asulay <fer.guitar.ln@gmail.com>
2020-05-24 09:28:00 +0200

    added articles filter

--- src/app/private/articles/articles.model.ts
+++ src/app/private/articles/articles.model.ts
@@ -3,12 +3,23 @@ import { TextFieldModel } from 'src/app/lib/models/text-field.model';
 import { RadioToggleModel } from 'src/app/lib/models/radio-toggle.model';
 const SEARCH_BY_CODE_LABEL = 'Buscar por codigo...';
 const SEARCH_BY_DESCRIPTION_LABEL = 'Buscar por descripcion...';
+const SEARCH_BY_CATEOGORY_LABEL = 'Buscar por rubro...';
 export class ArticlesModel extends BaseModel {
   constructor() {
     super();
     this.init();
     const sub = this.searchByField.valueChanges$.subscribe(value => {
-        this.searchField.placeholder = value === 'codeString' ? SEARCH_BY_CODE_LABEL : SEARCH_BY_DESCRIPTION_LABEL;
+      switch (value) {
+        case 'codeString':
+          this.searchField.placeholder = SEARCH_BY_CODE_LABEL;
+          break;
+        case 'description':
+          this.searchField.placeholder = SEARCH_BY_DESCRIPTION_LABEL;
+          break;
+        case 'category.description':
+          this.searchField.placeholder = SEARCH_BY_CATEOGORY_LABEL;
+          break;
+      }
     });
     this.subscriptions.push(sub);
   }
@@ -23,6 +34,7 @@ export class ArticlesModel extends BaseModel {
     options: [
       { text: 'Codigo', value: 'codeString' },
       { text: 'Descripcion', value: 'description' },
+      { text: 'Rubro', value: 'category.description' }
     ],
     textGetter: option => option.text,
   });
